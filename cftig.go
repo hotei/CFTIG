fd951d6bf51b1f6fadbae50b145b86c8c193a9f8
David Rook <hotei1352@gmail.com>
2014-03-24 22:50:09 -0400

    add cftig.go tookit bundler

---
+++ cftig.go
@@ -0,0 +1,16 @@
+// cftig.go (c) 2013-2014 David Rook - all rights reserved
+
+// go get github.com/hotei/CFTIG should import other programs as side effect
+// details can be found in the README.md files for each separate project
+
+package main
+
+import (
+	_ "github.com/hotei/mdr"	// utility function pkg
+	_ "github.com/hotei/bits"	// test import
+	)
+
+
+func main() {
+	
+}
